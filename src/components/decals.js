c7310de2a9c68fbe69741adb2c457a7e895501cd
sathyajith Dissanayake <slakmal68@gmail.com>
2020-03-07 10:46:04 +0530

    before eject

---
+++ src/components/decals.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import THREE from 'react-three';
+
+class Decal extends React.Component {
+
+    componentDidMount(){
+
+
+    }
+
+
+
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+export default Decal;
